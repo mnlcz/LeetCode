d039e0ee3b9b634dc893486ee681cc6938873bbc
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-11-24 17:56:26 -0300

    Problem 128
    Title: Best Time to Buy and Sell Stock

---
+++ 121.cs
@@ -0,0 +1,44 @@
+namespace _121;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var (possible, output) = Lazy(prices);
+        if (possible)
+            return output;
+
+        var l = 0;
+        var r = 1;
+        var max = 0;
+        while (true)
+        {
+            if (r == prices.Length)
+                break;
+
+            if (prices[l] < prices[r])
+            {
+                var profit = prices[r] - prices[l];
+                if (profit > max)
+                    max = profit;
+                r++;
+            }
+            else if (prices[l] >= prices[r])
+            {
+                l = r;
+                r++;
+            }
+        }
+        return max;
+    }
+
+    private static (bool possible, int output) Lazy(int[] prices)
+    {
+        return prices.Length switch
+        {
+            1 => (true, 0),
+            2 => prices[1] > prices[0] ? (true, prices[1] - prices[0]) : (false, -1),
+            _ => (false, -1)
+        };
+    }
+}

d58550e984ce6641bcdf9308c272c7a8f8e5c068
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-05-04 23:34:46 -0300

    LeetCode 75 Day 2

---
+++ Csharp/Challenges/392.cs
@@ -0,0 +1,20 @@
+namespace Csharp.Challenges;
+
+public static partial class Solution
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        if (s == t || s.Length == 0) return true;
+        if (t.Length == 0) return false;
+        var chIdx = 0;
+        foreach (var c in t)
+        {
+            var expected = s[chIdx];
+            if (c != expected) continue;
+            chIdx++;
+            if (chIdx == s.Length) return true;
+        }
+        
+        return false;
+    }
+}

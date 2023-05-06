05a3352e036548a166ef44c53dcdf04fa0ff4ba0
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-05-06 03:46:30 -0300

    LeetCode 75 Day 3

---
+++ Csharp/Challenges/LeetCode75/206.cs
@@ -0,0 +1,44 @@
+using Csharp.PredefinedClasses;
+
+namespace Csharp.Challenges.LeetCode75;
+
+public static partial class Solution
+{
+    public static ListNode? ReverseList(ListNode? head)
+    {
+        if (head is null) return null;
+        var list = new List<int>();
+        var it = head;
+        while (it is not null)
+        {
+            list.Add(it.val);
+            it = it.next;
+        }
+
+        var newHead = new ListNode(list[^1]);
+        it = newHead;
+        for (var i = list.Count - 2; i >= 0; i--)
+        {
+            var node = new ListNode(list[i]);
+            it.next = node;
+            it = it.next;
+        }
+
+        return newHead;
+    }
+
+    public static ListNode? ReverseListOptimized(ListNode? head)
+    {
+        ListNode? prev = null;
+        var act = head;
+        while (act is not null)
+        {
+            var temp = act.next;
+            act.next = prev;
+            prev = act;
+            act = temp;
+        }
+
+        return prev;
+    }
+}

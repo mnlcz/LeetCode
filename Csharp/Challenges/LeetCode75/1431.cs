9bb757f1431a17d6590e7f1532459e479c9561df
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-08-28 04:03:50 -0300

    https://leetcode.com/problems/kids-with-the-greatest-number-of-candies

---
+++ Csharp/Challenges/LeetCode75/1431.cs
@@ -0,0 +1,17 @@
+﻿namespace Csharp.Challenges.LeetCode75;
+public static partial class Solution
+{
+	public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+	{
+		var max = candies.Max();
+		var output = new bool[candies.Length];
+
+		for (var i = 0; i < candies.Length; i++) 
+		{
+			if (candies[i] + extraCandies >= max)
+				output[i] = true;
+		}
+
+		return output;
+	}
+}

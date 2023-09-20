559e84e62585c006d63fcb55db9214fa6b81e5a0
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-09-19 21:58:54 -0300

    Mejora

--- JS/NeetCode roadmap/Arrays & Hashing/242-valid-anagram.js
+++ JS/NeetCode roadmap/Arrays & Hashing/242-valid-anagram.js
@@ -2,11 +2,10 @@ let isAnagram = (s, t) => {
   if (s.length !== t.length) return false
   let reps1 = {}
   let reps2 = {}
-  for (let c of s) {
-    reps1[c] = c in reps1 ? reps1[c] + 1 : 1
-  }
-  for (let c of t) {
-    reps2[c] = c in reps2 ? reps2[c] + 1 : 1
+  for (let i = 0; i < s.length; i++) {
+    let c1 = s[i], c2 = t[i]
+    reps1[c1] = c1 in reps1 ? reps1[c1] + 1 : 1
+    reps2[c2] = c2 in reps2 ? reps2[c2] + 1 : 1
   }
   for (c in reps1) {
     if (reps1[c] !== reps2[c]) return false

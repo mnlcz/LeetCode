86175af842fb1bbff6b5dd7232d90461ff671060
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-09-19 22:16:56 -0300

    Objeto a Map
    Represento un diccionario mediante Map, optimización de 59ms a 55ms

--- JS/NeetCode roadmap/Arrays & Hashing/242-valid-anagram.js
+++ JS/NeetCode roadmap/Arrays & Hashing/242-valid-anagram.js
@@ -1,14 +1,14 @@
 let isAnagram = (s, t) => {
   if (s.length !== t.length) return false
-  let reps1 = {}
-  let reps2 = {}
+  let reps1 = new Map()
+  let reps2 = new Map()
   for (let i = 0; i < s.length; i++) {
     let c1 = s[i], c2 = t[i]
-    reps1[c1] = c1 in reps1 ? reps1[c1] + 1 : 1
-    reps2[c2] = c2 in reps2 ? reps2[c2] + 1 : 1
+    reps1.set(c1, reps1.has(c1) ? reps1.get(c1) + 1 : 1)
+    reps2.set(c2, reps2.has(c2) ? reps2.get(c2) + 1 : 1)
   }
-  for (c in reps1) {
-    if (reps1[c] !== reps2[c]) return false
-  }
+  for (const [key, val] of reps1)
+    if (val !== reps2.get(key)) 
+      return false
   return true
 }

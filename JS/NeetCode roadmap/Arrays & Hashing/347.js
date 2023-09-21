817ea24be73b215ca03b0e9041092ec3b671305b
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-09-20 22:19:16 -0300

    Cleanup

--- JS/NeetCode roadmap/Arrays & Hashing/347.js
+++ JS/NeetCode roadmap/Arrays & Hashing/347.js
@@ -9,6 +9,3 @@ const topKFrequent = (nums, k) => {
   })
   return [...reps.entries()].sort((a, b) => b[1] - a[1]).slice(0, k).map(x => x[0])
 }
-
-// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
-console.log(topKFrequent([3,0,1,0], 1))

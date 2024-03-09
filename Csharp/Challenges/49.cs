839599cc821644af1351847467b7b8650d03d4fe
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-03-08 21:14:12 -0300

    https://leetcode.com/problems/group-anagrams/

--- Csharp/Challenges/49.cs
+++ Csharp/Challenges/49.cs
@@ -4,6 +4,27 @@ public static partial class Solution
 {
     public static IList<IList<string>> GroupAnagrams(string[] strs)
     {
-        throw new NotImplementedException();
+        var groups = new Dictionary<string, IList<string>>();
+
+        foreach (var str in strs)
+        {
+            var sorted = SortString(str);
+
+            if (groups.ContainsKey(sorted))
+                groups[sorted].Add(str);
+            else
+                groups[sorted] = new List<string>() {str};
+        }
+
+        return groups.Values.ToList();
+    }
+
+    private static string SortString(string str)
+    {
+        var chars = str.ToCharArray();
+
+        Array.Sort(chars);
+        
+        return new string(chars);
     }
 }
